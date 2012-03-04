e95c77c09023d28e2dd774ab282acd7031a59172
Louis Salin <louis.phil@gmail.com>
2012-03-03 20:17:51 -0600

    use proper field method

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,11 +1,12 @@
 class Tweet
   include Mongoid::Document
-  fields :text, :type => String
-  fields :pruned_text, :type => String
-  fields :originator, :type => String
-  fields :reply_to, :type => String
-  fields :retweet_count, :type => Integer
-  fields :urls, :type => Hash
-  fields :is_my_reply, :type => Boolean
-  fields :mentions, :type => Hash
+
+  field :text, :type => String
+  field :pruned_text, :type => String
+  field :originator, :type => String
+  field :reply_to, :type => String
+  field :retweet_count, :type => Integer
+  field :urls, :type => Hash
+  field :is_my_reply, :type => Boolean
+  field :mentions, :type => Hash
 end
